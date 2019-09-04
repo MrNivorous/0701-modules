c4d3502a945e9edcd2735607014b2c319060bdad
MrNivorous <karn.sood@gmail.com>
2019-09-04 12:16:02 +0700

    070102

--- data.js
+++ data.js
@@ -1,7 +1,9 @@
 let items = [
-	{name: mango, price: 2},
-	{name: orange, price: 3},
-	{name: watermelon, price: 1}
+	{name: "mango", price: 2},
+	{name: "orange", price: 3},
+	{name: "watermelon", price: 1}
 ]
+
+
 
 module.exports = items
